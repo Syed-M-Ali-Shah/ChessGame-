c1094e2e934a8e4b37811b0719096ce8e3064adb
Syed-M-Ali-Shah <syedmalishah06@gmail.com>
2021-03-27 04:25:57 -0700

    chess

---
+++ ChessProject/Assets/Geometry.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Geometry
+{
+    static public Vector3 PointFromGrid(Vector2Int gridPoint)
+    {
+        float x = -3f + -2f * gridPoint.x;
+        float z = 2f + -2f * gridPoint.y;
+        return new Vector3(x, 0, z);
+    }
+
+    static public Vector2Int GridPoint(int col, int row)
+    {
+        return new Vector2Int(col, row);
+    }
+
+    static public Vector2Int GridFromPoint(Vector3 point)
+    {
+        int col = Mathf.FloorToInt(4f + point.x);
+        int row = Mathf.FloorToInt(4f + point.z);
+        return new Vector2Int(col, row);
+    }
+}
---
+++ ChessProject/Assets/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PieceType { King, Queen, Bishop, Knight, Rook, Pawn };
+
+public class Piece : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
